5f139d88ef0928a4e6f0524eea0c501696a00de3
Austin Condiff <austin.condiff@gmail.com>
2023-03-28 00:50:47 -0500

    Initial Commit

---
+++ Snake/SnakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeApp.swift
+//  Snake
+//
+//  Created by Austin Condiff on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct SnakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
